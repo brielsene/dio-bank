51c31551391b59403e6475aaec1e290f8d8b4167
Gabriel Sene <sene300@gmail.com>
2023-10-16 16:16:05 -0300

    first commit

---
+++ dio-bank/src/main/java/br/com/diobank/DioBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.diobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBankApplication.class, args);
+	}
+
+}
---
+++ dio-bank/src/test/java/br/com/diobank/DioBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
