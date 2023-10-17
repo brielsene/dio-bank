e2ee70426a756fdfb5b19fffd2962129d5e7db62
Gabriel Sene <sene300@gmail.com>
2023-10-16 22:38:05 -0300

    feat: Adding Configs

--- dio-bank/src/main/java/br/com/diobank/DioBankApplication.java
+++ dio-bank/src/main/java/br/com/diobank/DioBankApplication.java
@@ -2,8 +2,10 @@ package br.com.diobank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+
 public class DioBankApplication {
 
 	public static void main(String[] args) {
