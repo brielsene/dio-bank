c988e621da8e96103c841ced5f9228ff55e7edb7
Gabriel Sene <sene300@gmail.com>
2023-10-19 11:34:31 -0300

    fix: Add Serializable Json

--- dio-bank/src/main/java/br/com/diobank/model/User.java
+++ dio-bank/src/main/java/br/com/diobank/model/User.java
@@ -1,6 +1,7 @@
 package br.com.diobank.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,11 +24,11 @@ public class User {
     private String senha;
     private String email;
 
-    @JsonIgnore
+    @JsonManagedReference
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Account account;
 
-    @JsonIgnore
+    @JsonManagedReference
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Card>cards;
 }
